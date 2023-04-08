ac6a01188ecec06b397680014e309a264856f092
annasyun <yunjbly2470@naver.com>
2023-04-09 00:46:50 +0900

    feat: 저장버튼 클릭시 wiki item update 기능

--- src/api/wiki.js
+++ src/api/wiki.js
@@ -4,3 +4,8 @@ export const getWikiAxios = async () => {
   const response = await axios.get("http://localhost:5000/data");
   return response.data;
 };
+
+export const updateWikiAxios = async (id, body) => {
+  const response = await axios.put(`http://localhost:5000/data/${id}`, body);
+  return response.data;
+};
--- src/components/WikiItem.jsx
+++ src/components/WikiItem.jsx
@@ -1,9 +1,9 @@
 import { useParams } from "react-router-dom";
-import { getWikiAxios } from "../api/wiki";
+import { getWikiAxios, updateWikiAxios } from "../api/wiki";
 import { useState, useEffect } from "react";
 import ItemTitle from "./ItemTitle";
 import ItemContent from "./ItemContent";
-import EditWikiItem from "./EditWikiItem";
+import useInput from "../hooks/useInput";
 
 const WikiItem = () => {
   const { id } = useParams();
@@ -14,8 +14,10 @@ const WikiItem = () => {
     const foundListItem = wikiList.find((item) => item.id === Number(id));
     setListItem(foundListItem);
   };
-
+  const [editTitle] = useInput("");
+  const [editContent] = useInput("");
   const [editable, setEditable] = useState(false);
+
   const handleEdit = () => {
     setEditable(!editable);
   };
@@ -25,21 +27,50 @@ const WikiItem = () => {
     findListItem();
   }, [id]);
 
+  useEffect(() => {
+    if (listItem) {
+      editTitle.setValue(listItem.title);
+      editContent.setValue(listItem.content);
+    }
+  }, [listItem]);
+
   if (!listItem) {
     return <div>Loading...</div>;
   }
+
+  const updateWiki = async () => {
+    await updateWikiAxios(id, {
+      title: editTitle.value,
+      content: editContent.value,
+    });
+    const updatedItem = {
+      ...listItem,
+      title: editTitle.value,
+      content: editContent.value,
+    };
+    setListItem(updatedItem);
+    setEditable(false);
+  };
 
   return (
     <div>
       {editable ? (
         <>
-          <EditWikiItem
-            id={listItem.id}
-            title={listItem.title}
-            content={listItem.content}
+          <input
+            type="text"
+            value={editTitle.value}
+            onChange={editTitle.onchange}
           />
-          <button type="button" onClick={handleEdit}>
-            수정
+          <textarea
+            value={editContent.value}
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            onChange={editContent.onchange}
+          ></textarea>
+          <button type="button" onClick={updateWiki}>
+            저장
           </button>
         </>
       ) : (
